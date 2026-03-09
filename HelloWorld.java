722f433a606ef6c3cd0ae01db60a80c46926f3cb
jatinbhalla <jatinbhalla18.jb@gmail.com>
2026-03-09 15:19:26 +0530

    sample java

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java GitHub Actions!");
+        
+        // Display system information
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("Operating System: " + System.getProperty("os.name"));
+        
+        // Simple calculation example
+        int sum = calculateSum(10, 20);
+        System.out.println("Sum of 10 and 20 is: " + sum);
+    }
+    
+    public static int calculateSum(int a, int b) {
+        return a + b;
+    }
+}
